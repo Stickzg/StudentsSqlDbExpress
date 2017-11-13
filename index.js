4d1ea5ae3d3d97ff7960ff8934fefa3673f10bbd
Stickz <vastag.balint@gmail.com>
2017-11-13 13:14:34 +0100

    added model object

--- index.js
+++ index.js
@@ -6,29 +6,12 @@ const hbs = require('hbs');
 const methodOverride = require('method-override');
 const path = require('path');
 
-require('dotenv').config();
-const mysql = require('mysql');
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE
-});
-
-connection.connect();
-const studentsTable = `create table if not exists students
-    (id int AUTO_INCREMENT,
-    name varchar(100),
-    sex boolean,
-    score int,
-    age int,
-    PRIMARY KEY(id) );`;
-connection.query(studentsTable, function (err, rows, fields) {
-  if (err) throw err;
-  console.log('Students table created.');
-});
-
+const Model = require('./model');
+const Student = new Model();
 // handlebars
+
+Student.connect();
+Student.init();
 
 app.set('views', './views/');
 app.set('view engine', 'hbs');
@@ -47,74 +30,43 @@ hbs.registerHelper('currentPage', (pages, page, options) => {
   return options.inverse(this);
 });
 
-app.get('/students/create', function (request, response) {
-  response.render('students/create');
-});
+// student count
 
-app.get('/students/:id/update', function (request, response) {
-  const studentDatas = `SELECT * FROM students WHERE id='${request.params.id}' ;`;
-
-  connection.query(studentDatas, function (err, rows, fields) {
-    if (err) throw err;
-
-    if (rows && rows.length > 0) {
-      const student = rows[0];
-
-      response.format({
-        'text/html': function () {
-          response.render('students/update', {
-            student: student
-          });
-        },
-        'applicaton/json': function () {
-          response.json({student: student});
-        }
-      });
+app.get('/students/count', function (request, response) {
+  Student.count(function (err, students) {
+    if (err) {
+      response.render('errors/500', {error: err});
     } else {
-      response.format({
-        'text/html': function () {
-          response.render('students/update', {
-            student: []
-          });
-        },
-        'applicaton/json': function () {
-          response.json({ student: [] });
-        }
-      });
+      response.json({count: students});
     }
   });
 });
 
-app.get('/students', function (request, response) {
-  const list = `SELECT * FROM students;`;
-  connection.query(list, function (err, rows, fields) {
-    if (rows && rows.length > 0) {
-      const students = rows;
-      response.format({
-        'text/html': function () {
-          response.render('students/index', {
-            students: students
-          });
-        },
-        'applicaton/json': function () {
-          response.json({students: students});
-        }
-      });
-    } else {
-      response.format({
-        'text/html': function () {
-          response.render('students/index', {
-            students: []
-          });
-        },
-        'applicaton/json': function () {
-          response.json({ students: [] });
-        }
-      });
-    }
+app.get('/students/create', function (request, response) {
+  response.render('students/create');
+});
 
-    if (err) throw err;
-    console.log('Students table created.');
+// student update page
+
+app.get('/students/:id/update', function (request, response) {
+  Student.findOne({id: request.params.id}, function (err, student) {
+    if (err) {
+      response.render('errors/500', {error: err});
+    } else {
+      response.render('students/update', {student: student});
+    }
+  });
+});
+
+// List all students
+app.get('/students', function (request, response) {
+  Student.findAll(function (error, students) {
+    if (error) {
+      response.render('errors/500', {error: error}
+      );
+    } else {
+      response.render('students/index', {students: students});
+    }
   });
 });
 
@@ -128,91 +80,51 @@ app.get('/scores', function (request, response) {
   response.json({student: interval});
 });
 
+// repace student
+
 app.put('/students/:id', function (request, response) {
-  const changeStudent = `UPDATE students SET name='${request.body.name}', sex='${request.body.sex}',
-    score='${request.body.score}', age='${request.body.age}' WHERE id='${request.params.id}';`;
-
-  connection.query(changeStudent, function (err, rows, fields) {
-    const StudentID = request.params.id;
-
-    if (err) throw err;
-
-    response.format({
-      'text/html': function () {
-        response.redirect('/students/' + StudentID);
-      },
-
-      'application/json': function () {
-        response.redirect('/students/' + StudentID);
-      }
-
-    });
-  });
-});
-
-app.get('/students/:id', function (request, response) {
-  const studentShow = `SELECT * FROM students WHERE id='${request.params.id}' ;`;
-
-  connection.query(studentShow, function (err, rows, fields) {
-    if (err) throw err;
-
-    if (rows && rows.length > 0) {
-      const student = rows[0];
-
-      response.format({
-        'text/html': function () {
-          response.render('students/show', {
-            student: student
-          });
-        },
-        'applicaton/json': function () {
-          response.json({student: student});
-        }
-      });
+  Student.update(request.params.id, request.body, function (err, student) {
+    if (err) {
+      response.render('errors/500', {error: err});
     } else {
-      response.format({
-        'text/html': function () {
-          response.render('students/show', {
-            student: []
-          });
-        },
-        'applicaton/json': function () {
-          response.json({ student: [] });
-        }
-      });
+      response.redirect('/  students/');
     }
   });
 });
 
-app.delete('/students/:id', function (request, response) {
-  const deleteStudent = `DELETE FROM students WHERE id='${request.params.id}';`;
-  connection.query(deleteStudent, function (err, rows, fields) {
-    if (err) throw err;
+// get student by id
 
-    response.format({
-      'text/html': () => response.redirect('/students'),
-      'applicaton/json': () => response.redirect('/students')
-    });
-  }
-  );
+app.get('/students/:id', function (request, response) {
+  Student.findOne({id: request.params.id}, function (err, student) {
+    if (err) {
+      response.render('errors/500', {error: err});
+    } else {
+      response.render('students/show', {student: student});
+    }
+  });
 });
 
+// delete student
+
+app.delete('/students/:id', function (request, response) {
+  Student.delete(request.params.id, function (err, student) {
+    if (err) {
+      response.render('errors/500', {error: err});
+    } else {
+      response.redirect('/students/');
+    }
+  });
+});
+
+// create Student
+
 app.post('/students', function (request, response) {
-  const createStudent = `INSERT INTO students (name, sex, score, age)
-  VALUES ('${request.body.name}' ,
-     '${request.body.sex}', 
-     '${request.body.score}', 
-     '${request.body.age}' ) ;`;
-  connection.query(createStudent, function (err, rows, fields) {
-    if (err) throw err;
-    response.format({
-      'text/html': function () {
-        response.redirect('/students');
-      },
-      'application/json': function () {
-        response.redirect('/students');
-      }
-    });
+  Student.create(request.body, function (err, student) {
+    if (err) {
+      response.render('errors/500', {error: err});
+    } else {
+      response.redirect('/students/');
+    }
   });
 });
 
